ea0d20883a53886e29de28413404c5ef94a5d29a
Anastasia <tildatopdev@gmail.com>
2024-07-22 17:19:37 +0300

    add file task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btn = document.querySelector(".change-color");
+const textColor = document.querySelector(".color");
+btn.addEventListener("click", (event) => {
+  let newColor = getRandomHexColor();
+  btn.style.backgroundColor = newColor;
+  textColor.style.color = newColor;
+});
