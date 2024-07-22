34d81a9a7f92b371cd4e6a689f244a3168a0f358
Anastasia <tildatopdev@gmail.com>
2024-07-22 17:02:55 +0300

    add file task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const emailFind = document.querySelector(".login-form");
+emailFind.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const email = event.target.elements.email.value.trim();
+  const password = event.target.password.value.trim();
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+  } else {
+    const formInformation = {
+      email: email,
+      password: password,
+    };
+    console.log(formInformation);
+    event.currentTarget.reset();
+  }
+});
