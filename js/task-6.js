1824fda58be72a44884b0c5279bc0915392c1b91
Anastasia <tildatopdev@gmail.com>
2024-07-22 18:26:58 +0300

    add task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+function createBoxes(amount) {
+  const boxesContainer = document.querySelector("#boxes");
+  boxesContainer.innerHTML = ""; // Очищення попередніх елементів
+
+  const boxSizeIncrement = 10; // Збільшення розміру кожного наступного елемента
+  const initialSize = 30; // Розмір першого елемента
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    const size = initialSize + i * boxSizeIncrement;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.margin = "44px"; // Додаємо відступ між елементами
+    boxesContainer.append(box);
+  }
+}
+
+function destroyBoxes() {
+  document.querySelector("#boxes").innerHTML = ""; // Очищення вмісту
+}
+
+document.querySelector("[data-create]").addEventListener("click", () => {
+  const input = document.querySelector("#controls input");
+  const amount = Number(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  }
+  input.value = ""; // Очищення значення в інпуті
+});
+
+document
+  .querySelector("[data-destroy]")
+  .addEventListener("click", destroyBoxes);
